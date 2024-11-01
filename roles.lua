c2cbf2830f716ce0f4c50cc5e52f2b12dd0caa6c
jaz <reed.kille+git@gmail.com>
2024-11-01 12:30:13 -0500

    Create roles.lua

---
+++ roles.lua
@@ -0,0 +1,21 @@
+local colorRoles = {}
+
+colorRoles.colorIndex = {
+	["leadDeveloper"] = Color3.fromRGB(57, 0, 103),
+	["highDeveloper"] = Color3.fromRGB(0, 55, 255),
+	["feedbackTeam"] = Color3.fromRGB(255, 235, 0)
+}
+
+colorRoles.userIndex = {
+	["%-278161083"] = "leadDeveloper", -- // jazzful
+	
+	["%-1376918790"] = "highDeveloper", -- // AsianMoretti
+	["%-206924927"] = "highDeveloper", -- // alauddin123
+	["%-40919030"] = "highDeveloper", -- // EagleEatsPies
+
+	["%-209206837"] = "feedbackTeam", -- // ColossalCorgis
+	["%-116300454"] = "feedbackTeam", -- // AutumnBrotherhood
+	["%-49665104"] = "feedbackTeam", -- // robloxdreamer969
+}
+
+return colorRoles
