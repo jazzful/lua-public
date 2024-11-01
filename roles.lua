cea1db9e044e49866b82ab567faa5491cbf25710
jaz <reed.kille+git@gmail.com>
2024-11-01 13:49:29 -0500

    Update roles.lua

--- roles.lua
+++ roles.lua
@@ -4,11 +4,15 @@ colorRoles.colorIndex = {
 	-- // {Main, Accent}
 	["dev_lead"] = {Color3.fromRGB(0, 229, 255), Color3.fromRGB(108, 255, 130)},
 	["dev_high"] = {Color3.fromRGB(255, 148, 150), Color3.fromRGB(255, 204, 0)},
+	["dev_artist"] = {Color3.fromRGB(137, 255, 180), Color3.fromRGB(79, 255, 255)},
+	
 	["team_feedback"] = {Color3.fromRGB(255, 221, 152), Color3.fromRGB(250, 255, 205)}
 }
 
 colorRoles.userIndex = {
 	["278161083"] = "dev_lead", -- // jazzful
+
+	["81232760"] = "dev_artist", -- / PaleoBlue
 	
 	["1376918790"] = "dev_high", -- // AsianMoretti
 	["206924927"] = "dev_high", -- // alauddin123
@@ -21,7 +25,6 @@ colorRoles.userIndex = {
 	["-1"] = "dev_lead",
 	["-2"] = "dev_high",
 	["-3"] = "team_feedback",
-
 }
 
 return colorRoles
