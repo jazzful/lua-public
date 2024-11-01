bae9bd94b05530273fa904c1e176cc8d72f0af7d
jaz <reed.kille+git@gmail.com>
2024-11-01 13:16:50 -0500

    Update roles.lua

--- roles.lua
+++ roles.lua
@@ -1,21 +1,22 @@
 local colorRoles = {}
 
 colorRoles.colorIndex = {
-	["leadDeveloper"] = Color3.fromRGB(57, 0, 103),
-	["highDeveloper"] = Color3.fromRGB(0, 55, 255),
-	["feedbackTeam"] = Color3.fromRGB(255, 235, 0)
+	-- // {Main, Accent}
+	["dev_lead"] = {Color3.fromRGB(0, 229, 255), Color3.fromRGB(108, 255, 130)},
+	["dev_high"] = {Color3.fromRGB(255, 148, 150), Color3.fromRGB(255, 204, 0)},
+	["team_feedback"] = {Color3.fromRGB(255, 221, 152), Color3.fromRGB(250, 255, 205)}
 }
 
 colorRoles.userIndex = {
-	["278161083"] = "leadDeveloper", -- // jazzful
+	["278161083"] = "dev_lead", -- // jazzful
 	
-	["1376918790"] = "highDeveloper", -- // AsianMoretti
-	["206924927"] = "highDeveloper", -- // alauddin123
-	["40919030"] = "highDeveloper", -- // EagleEatsPies
+	["1376918790"] = "dev_high", -- // AsianMoretti
+	["206924927"] = "dev_high", -- // alauddin123
+	["40919030"] = "dev_high", -- // EagleEatsPies
 
-	["209206837"] = "feedbackTeam", -- // ColossalCorgis
-	["116300454"] = "feedbackTeam", -- // AutumnBrotherhood
-	["49665104"] = "feedbackTeam", -- // robloxdreamer969
+	["209206837"] = "team_feedback", -- // ColossalCorgis
+	["116300454"] = "team_feedback", -- // AutumnBrotherhood
+	["49665104"] = "team_feedback", -- // robloxdreamer969
 }
 
 return colorRoles
